390e87e30c2f0b82838c3f2258be9b18da3b92f4
Jon Glass <jon.glass@camlingroup.com>
2025-03-12 15:13:35 +0000

    Convert AddGroupMemberRequest to be a record type and change expired_at from String to DateTime

--- src/GitLabApiClient/Models/Groups/Requests/AddGroupMemberRequest.cs
+++ src/GitLabApiClient/Models/Groups/Requests/AddGroupMemberRequest.cs
@@ -1,4 +1,4 @@
-using GitLabApiClient.Internal.Paths;
+using System;
 using Newtonsoft.Json;
 
 namespace GitLabApiClient.Models.Groups.Requests
@@ -6,34 +6,11 @@ namespace GitLabApiClient.Models.Groups.Requests
     /// <summary>
     /// Used to add members in a group.
     /// </summary>
-    public sealed class AddGroupMemberRequest
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AddGroupMemberRequest"/> class.
-        /// </summary>
-        /// <param name="accessLevel">The access level of the new member.</param>
-        public AddGroupMemberRequest(AccessLevel accessLevel, UserId userId)
-        {
-            AccessLevel = (int)accessLevel;
-            UserId = System.Convert.ToInt32(userId.ToString());
-        }
-
-        /// <summary>
-        /// The desired access level
-        /// </summary>
-        [JsonProperty("access_level")]
-        public int AccessLevel { get; private set; }
-
-        /// <summary>
-        /// The UserID to add
-        /// </summary>
-        [JsonProperty("user_id")]
-        public int UserId { get; private set; }
-
-        /// <summary>
-        /// The membership expiration date. Date time string in the format YEAR-MONTH-DAY, e.g. 2016-03-11.
-        /// </summary>
-        [JsonProperty("expires_at")]
-        public string ExpiresAt { get; set; }
-    }
+    /// <param name="AccessLevel"> The desired access level </param>
+    /// <param name="UserId"> The UserID to add </param>
+    /// <param name="ExpiresAt"> The membership expiration date. Date time string in the format YEAR-MONTH-DAY, e.g. 2016-03-11. </param>
+    public sealed record AddGroupMemberRequest(
+        [property:JsonProperty("access_level")] int AccessLevel,
+        [property:JsonProperty("user_id")] int UserId,
+        [property:JsonProperty("expires_at")] DateTime ExpiresAt);
 }
